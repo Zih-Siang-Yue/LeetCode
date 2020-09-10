86a96fa516b2b62c954941ff9a90cba0e14c4c75
Zih-Siang Yue <fdgo555@yahoo.com.tw>
2020-09-10 22:59:00 +0800

    feat: New class define the TreeNode

---
+++ Leetcode/Definition.swift
@@ -0,0 +1,23 @@
+//
+//  Definition.swift
+//  Leetcode
+//
+//  Created by Zih-Siang Yue on 2020/9/7.
+//  Copyright © 2020 Sean. All rights reserved.
+//
+
+import Foundation
+
+//Definition for a binary tree node.
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
--- Leetcode/LeetcodeQuestion/Leetcode.swift
+++ Leetcode/LeetcodeQuestion/Leetcode.swift
@@ -984,19 +984,6 @@ class Leetcode {
         return isSameTree(p?.left, q?.left) && isSameTree(p?.right, q?.right)
     }
     
-    //Leetcode 104 Maximum Depth of Binary Tree
-    public class TreeNode {
-        public var val: Int
-        public var left: TreeNode?
-        public var right: TreeNode?
-        public init(_ val: Int) {
-            self.val = val
-            self.left = nil
-            self.right = nil
-        }
-    }
-    
-    
     func maxDepth(_ root: TreeNode?) -> Int {
         if root?.left == nil && root?.right == nil {
             return 1

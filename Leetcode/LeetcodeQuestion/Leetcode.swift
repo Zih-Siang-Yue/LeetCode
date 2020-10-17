1355fb61739b1bbd5a0db3715084d30fb0eaa2a0
Zih-Siang Yue <fdgo555@yahoo.com.tw>
2020-10-17 11:56:17 +0800

    feat: Q16 is solved

--- Leetcode/LeetcodeQuestion/Leetcode.swift
+++ Leetcode/LeetcodeQuestion/Leetcode.swift
@@ -94,6 +94,10 @@ class Leetcode {
         let longestPrefix:String = longestCommonPrefix(["leetcode", "leet", "lee", "lean"])
         print("leetcode14 ouput: \(longestPrefix)")
         
+        //16
+        let threeSum:Int = threeSumClosest([1, 1, 1 ,1], -100)
+        print("leetcode16 output: \(threeSum)")
+        
         //20
         let isValid = isValidParentheses("[](){}")
         print("leetcode20 output: \(isValid)")
@@ -776,6 +780,32 @@ class Leetcode {
         }
         
         return longestPrefix
+    }
+    
+    //Leetcode - 16 3Sum Closest
+    func threeSumClosest(_ nums: [Int], _ target: Int) -> Int {
+        return threeSumClosestSolution1(nums, target)
+    }
+    
+    func threeSumClosestSolution1(_ nums: [Int], _ target: Int) -> Int {
+        var threeSum:Int? = nil
+        let length = nums.count
+        
+        for i in 0 ..< length {
+            for j in i + 1 ..< length   {
+                for k in j + 1 ..< length {
+                    let sum = nums[i] + nums[j] + nums[k]
+                    let interval = sum - target
+                    if interval == 0 { return sum }
+                    if threeSum == nil {
+                        threeSum = sum
+                        continue
+                    }
+                    threeSum = abs(threeSum! - target) < abs(interval) ? threeSum : sum
+                }
+            }
+        }
+        return threeSum!
     }
     
     //Leetcode - 20
